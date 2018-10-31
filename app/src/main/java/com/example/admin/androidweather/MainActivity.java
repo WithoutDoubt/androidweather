5f4ac9def0d788a760d53c467ec1ec899058c6b5
oney <1114303855@qq.com>
2018-10-31 22:55:59 +0800

    大家好

--- app/src/main/java/com/example/admin/androidweather/MainActivity.java
+++ app/src/main/java/com/example/admin/androidweather/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.admin.androidweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this,"点了一下",Toast.LENGTH_SHORT).show();
+                String data = "Hello SecondActivity";
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("extra_data",data);  //传递数据
+                startActivity(intent);
+            }
+        });
     }
 }
