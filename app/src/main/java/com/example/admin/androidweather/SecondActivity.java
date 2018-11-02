517fd4a0ec399972575e45265fa38854d43798d1
oney <1114303855@qq.com>
2018-11-02 16:29:33 +0800

    按钮按下颜色变化，引入swing库

--- app/src/main/java/com/example/admin/androidweather/SecondActivity.java
+++ app/src/main/java/com/example/admin/androidweather/SecondActivity.java
@@ -1,19 +1,34 @@
 package com.example.admin.androidweather;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class SecondActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
 
-        Intent intent = getIntent();
-        String data = intent.getStringExtra("extra_data");
-        Toast.makeText(SecondActivity.this,data,Toast.LENGTH_SHORT).show();
+        Button button2 = (Button)findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,ThirdActicity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
